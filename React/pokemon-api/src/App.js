bb337b1b4164bb4ce58501411037b291b76d3b15
Rubonic <dannykraemer099@gmail.com>
2022-05-10 11:06:28 -0400

    first commit

--- React/pokemon-api/src/App.js
+++ React/pokemon-api/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-// import Pokemon from './components/Pokemon';
-import PokemonWithAxios from './components/PokemonWithAxios';
+import Pokemon from './components/Pokemon';
+// import PokemonWithAxios from './components/PokemonWithAxios';
 
 function App() {
   return (
     <div className="App">
       <h1>Pokemon</h1>
-      {/* <Pokemon></Pokemon> */}
-      <PokemonWithAxios></PokemonWithAxios>
+      <Pokemon></Pokemon>
+      {/* <PokemonWithAxios></PokemonWithAxios> */}
     </div>
   );
 }
